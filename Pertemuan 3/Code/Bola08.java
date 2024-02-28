d393dee74e1be6692f6dda35d765f695737cd796
cindylrs04 <larasaticindy231@gmail.com>
2024-02-28 16:20:35 +0700

    Bola

---
+++ Pertemuan 3/Code/Bola08.java
@@ -0,0 +1,18 @@
+package Code;
+
+public class Bola08 {
+    int Jarijari;
+
+    Bola08(int j) {
+        Jarijari = j;
+    }
+
+    double hitungLuas() {
+        return 4 * 3.14 * Jarijari * Jarijari;
+    }
+
+    double hitungVolume() {
+        return 4 / 3 * 3.14 * Jarijari * Jarijari * Jarijari;
+    }
+    
+}
